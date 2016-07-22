5c7d6569d302f89648f09f9390d263d688bae052
philc <philc@CANNY>
2016-07-23 00:36:20 +0100

    Stripe attempt with a Few errors

---
+++ DesignUrFixie/Controllers/BikesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DesignUrFixie.Models;
+using Stripe;
+
+namespace DesignUrFixie.Controllers
+{
+    
+    public class BikesController : Controller
+    {
+        private MyDbContext db = new MyDbContext();   //create an instance of the DataContext class in our DB
+
+        // GET: Bikes
+        //[Authorize]
+        public ActionResult Index()
+        {
+            return View(db.Bikes.ToList());
+        }
+
+        // GET: Bikes/Details/5
+        //[Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+                   
+    }
+
+        // GET: /Payment/
+
+        public ActionResult Charge(string stripeToken, string stripeEmail)
+        {
+            string apiKey = "sk_test_3RiGWe2dnDwGiXGBS2F6iQ3m";
+            var stripeClient = new DesignUrFixie.StripeClient(apiKey);
+
+            dynamic response = stripeClient.CreateChargeWithToken(52500, stripeToken, "EUR", stripeEmail);
+
+            if (response.IsError == false && response.Paid)
+            {
+                // success
+            }
+
+            return View("Charge");
+
+        }
+       
+
+        // GET: Bikes/Create
+        //[AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bikes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BikeId,BikeName,FrameColour,SaddleColour,HandlebarColour,WheelColour")] Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bikes.Add(bike);
+                db.SaveChanges();
+                
+                return RedirectToAction("Index");
+            }
+
+            return View(bike);
+        }
+
+        // GET: Bikes/Edit/5
+        //[Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Bikes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BikeId,BikeName,FrameColour,SaddleColour,HandlebarColour,WheelColour")] Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bike).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bike);
+        }
+
+        // GET: Bikes/Delete/5
+        //[Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Bikes/Delete/5
+        //[Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bike bike = db.Bikes.Find(id);
+            db.Bikes.Remove(bike);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DesignUrFixie/Models/Bike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesignUrFixie.Models
+{
+    
+    public class Bike
+    {
+        public int BikeId { get; set; }
+        [Required]
+        public string BikeName { get; set; }
+        [Required]
+        public string FrameColour { get; set; }
+        [Required]
+        public string SaddleColour { get; set; }
+        [Required]
+        public string HandlebarColour { get; set; }
+        [Required]
+        public string WheelColour { get; set; }
+
+        internal static void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Add(Bike bike)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
